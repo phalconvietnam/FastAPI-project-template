0be248232434e1aa2d503c65ef379ebe82dac5d6
VDev <phalconvietnam@hotmail.com>
2021-04-22 16:33:43 +0700

    first commit

--- router/api_router.py
+++ router/api_router.py
@@ -5,5 +5,7 @@ from app.controllers import api_user, api_login, api_register
 router = APIRouter()
 
 router.include_router(api_login.router, tags=["login"], prefix="/login")
+
 router.include_router(api_register.router, tags=["register"], prefix="/register")
+
 router.include_router(api_user.router, tags=["user"], prefix="/users")
